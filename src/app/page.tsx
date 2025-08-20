85a67929addc05580ce7b8f67cba699c3c343fc4
ArnaudBay <arnaudbayale@gmail.com>
2025-08-20 18:42:50 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+      <div className="bg-green-600 p-5 rounded-4xl text-white text-3xl m-4 border-4 border-dashed  border-black">
+        Application
+        </div>
+  );
+}
